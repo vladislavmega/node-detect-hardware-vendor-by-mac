28f61410b8c2ef2dea98821ca1e9a05b4c615a5f
vladislavmega <vladislavmega@MacBook-Pro-vladislavmega.local>
2017-10-26 12:30:53 +0300

    map base as json

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ or
 usage: 
  
 `var detectByMac = require('detect-hardware-vendor-by-mac');
-detectByMac('dc:0b:34:a7:92:31'); // {key: 'dc:0b:34:a7:92:31', shortTitle: 'LgElectr', fullTitle: ' LG Electronics (Mobile Communications)'}`
+detectByMac('dc:0b:34:a7:92:31'); 
+// output be {key: 'dc:0b:34:a7:92:31', vendorMacPrefix: 'dc:0b:34', shortTitle: 'LgElectr', fullTitle: ' LG Electronics (Mobile Communications)'}`
   
 base taken from:
 https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf
--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 var fs = require('fs');
 var _ = require('lodash');
-var macBaseRaw = fs.readFileSync(__dirname + '/macbase.txt', {encoding: 'utf-8'});
-var macBase = macBaseRaw.split(/\r?\n/).map(function (line) {
-    var splitedLine = line.split(' ');
-    var fullTitle = '';
-    if (splitedLine.length > 2) {
-        fullTitle = splitedLine.reduce(function (prev, current, index, array) {
-            return (index < 2) ? '' : prev + ' ' + current;
-        });
-    }
-
-    return {
-        key: splitedLine[0].toLowerCase(),
-        shortTitle: splitedLine[1],
-        fullTitle: fullTitle.trim(),
-    }
-});
+var macBase = JSON.parse(fs.readFileSync(__dirname + '/base.json', {encoding: 'utf-8'}));
 
 module.exports = function(macAddress){
+    this.macs = macBase;
     var macSplit = macAddress.toLowerCase().split(':');
     var mac5 = macSplit.reduce(function(prev, current, index) {
         return (index <5) ? prev + ':'+ current : prev
@@ -33,6 +19,7 @@ module.exports = function(macAddress){
     if (!finded) finded = _.find(macBase, {key: mac3});
     return {
         key: macAddress,
+        vendorMacPrefix: (finded)? finded.key : '',
         shortTitle: (finded) ? finded.shortTitle : '',
         fullTitle: (finded) ? finded.fullTitle : '',
     }
